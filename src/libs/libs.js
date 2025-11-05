0f3fd73d07da66b78cba31d7a509df7c35a51d2d
mrh3llo <quocmrh3llo@gmail.com>
2025-11-05 23:54:43 +0700

    Cập nhật header, footer, bổ sung các thành phần giao diện

--- src/libs/libs.js
+++ src/libs/libs.js
@@ -7,9 +7,15 @@ function getDateTime() {
 
     const hours = today.getHours();
     const minutes = today.getMinutes();
-
+    
     let displayDate =
     `Việt Nam, ngày ${day} tháng ${month} năm ${year} <br> ${hours} : ${minutes}`;
+
+    // Nếu số phút và giờ nhỏ hơn 10 thì thêm số 0 vào trước. Ví dụ từ 8 : 5 thành 08 : 05
+    if(hours < 10 && minutes < 10) {
+        displayDate =
+        `Việt Nam, ngày ${day} tháng ${month} năm ${year} <br> 0${hours} : 0${minutes}`;
+    }
 
     document.getElementById('display_date_time').innerHTML = displayDate;
 }
