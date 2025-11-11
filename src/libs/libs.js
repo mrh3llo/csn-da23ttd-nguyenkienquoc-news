49026b8583e051939388ec9c60c2238939dc4259
mrh3llo <quocmrh3llo@gmail.com>
2025-11-12 02:43:54 +0700

    Sửa toàn bộ tệp thư viện thành phần components.css, và các tệp liên quan khác - version 1.2

--- src/libs/libs.js
+++ src/libs/libs.js
@@ -1,4 +1,6 @@
-function getDateTime() {
+// Hàm cập nhật thời gian theo thời gian thực
+{
+    function getDateTime() {
     const today = new Date();
 
     const day = today.getDate();
@@ -12,14 +14,27 @@ function getDateTime() {
     `Việt Nam, ngày ${day} tháng ${month} năm ${year} <br> ${hours} : ${minutes}`;
 
     // Nếu số phút và giờ nhỏ hơn 10 thì thêm số 0 vào trước. Ví dụ từ 8 : 5 thành 08 : 05
-    if(hours < 10 && minutes < 10) {
+    if(hours < 10 && minutes < 10)
         displayDate =
         `Việt Nam, ngày ${day} tháng ${month} năm ${year} <br> 0${hours} : 0${minutes}`;
+    else if(hours < 10)
+        displayDate =
+        `Việt Nam, ngày ${day} tháng ${month} năm ${year} <br> 0${hours} : ${minutes}`;
+    else if(minutes < 10)
+        displayDate =
+        `Việt Nam, ngày ${day} tháng ${month} năm ${year} <br> 0${hours} : 0${minutes}`;
+
+        document.getElementById('display_date_time').innerHTML = displayDate;
     }
 
-    document.getElementById('display_date_time').innerHTML = displayDate;
+    // Gọi hàm getDateTime mỗi 1000 mili giây, tức là 1 giây
+    setInterval(getDateTime, 60000);
+    getDateTime()
 }
 
-// Gọi hàm getDateTime mỗi 1000 mili giây, tức là 1 giây
-setInterval(getDateTime, 60000);
-getDateTime()
+// Đường link
+{
+    function jumpTo(path) {
+        window.location.href = path;
+    }
+}
