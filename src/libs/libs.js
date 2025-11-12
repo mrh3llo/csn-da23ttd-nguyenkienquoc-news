e5dac3297081a9df1774dd4205e8c377e461938e
mrh3llo <quocmrh3llo@gmail.com>
2025-11-13 01:02:02 +0700

    Cập nhật, bổ sung tệp sign_up.html, sign_in.html, forgot_password.html. Thêm thuật toán xử lý dữ liệu đăng nhâp, đăng ký. Bô sung những hiệu ứng nhỏ nhằm tăng tương tác với người dùng

--- src/libs/libs.js
+++ src/libs/libs.js
@@ -38,3 +38,77 @@
         window.location.href = path;
     }
 }
+
+// Xử lý đăng nhâp, đăng ký
+{
+    function validateSignUpForm(event) {
+        event.preventDefault();
+
+        let email = document.getElementById('user_email').value;
+        let name = document.getElementById('user_name').value;
+        let password = document.getElementById('user_password').value;
+        let confirm = document.getElementById('user_password_comfirm').value;
+
+        let errors = [];
+
+        // Kiểm tra dữ liệu không được để trống
+        if(!email) errors.push('Không để trống email');
+        if(!name) errors.push('Không được để trống tên người dùng');
+        if(!password) errors.push('Không được để trống mật khẩu');
+        if(!confirm) errors.push('Xin hãy xác nhận mật khẩu');
+
+        if(!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email))
+            errors.push('Email không hợp lệ!');
+        
+        if(password.length < 8)
+            errors.push('Mật khẩu phải có ít nhất 8 ký tự!');
+        if(/\s/.test(password))
+            errors.push('Mật khẩu không dược chứa khoảng trắng!');
+
+        if(confirm != password)
+            errors.push('Mật khẩu không trùng khớp!');
+
+        // Thông báo trạng thái
+        if(errors.length <= 0) {
+            setTimeout(() => {
+                document.getElementById('log_status').innerHTML = 'ĐĂNG NHẬP THÀNH CÔNG!!!';
+                console.log('Đăng nhập thành công');
+            }, 1000);
+        }
+        else {
+            document.getElementById('log_status').innerHTML = errors.join('<br>');
+        }
+    }
+
+    function validateSignInForm(event) {
+        event.preventDefault();
+
+        let email = document.getElementById('user_email').value;
+        let password = document.getElementById('user_password').value;
+
+        let errors = [];
+
+        // Kiểm tra dữ liệu không được để trống
+        if(!email) errors.push('Không để trống email');
+        if(!password) errors.push('Không được để trống mật khẩu');
+
+        if(!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email))
+            errors.push('Email không hợp lệ!');
+        
+        if(password.length < 8)
+            errors.push('Mật khẩu phải có ít nhất 8 ký tự!');
+        if(/\s/.test(password))
+            errors.push('Mật khẩu không dược chứa khoảng trắng!');
+
+        // Thông báo trạng thái
+        if(errors.length <= 0) {
+            setTimeout(() => {
+                document.getElementById('log_status').innerHTML = 'ĐĂNG NHẬP THÀNH CÔNG!!!';
+                console.log('Đăng nhập thành công');
+            }, 1000);
+        }
+        else {
+            document.getElementById('log_status').innerHTML = errors.join('<br>');
+        }
+    }
+}
